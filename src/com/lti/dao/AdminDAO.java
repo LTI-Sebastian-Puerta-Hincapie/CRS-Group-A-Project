81a1529b9f626666b58d0d3d81708b18469f7baf
jonathanyeh98 <yehjonathan98@gmail.com>
2022-09-21 10:23:38 -0400

    committing progress so far

--- src/com/lti/dao/AdminDAO.java
+++ src/com/lti/dao/AdminDAO.java
@@ -3,12 +3,23 @@
  */
 package com.lti.dao;
 
+import com.lti.bean.Course;
+import com.lti.bean.Professor;
+import com.lti.bean.Student;
+
 /**
  * @author user106
  *
  */
 public interface AdminDAO {
 
-	// TODO: define all respective method with database operations and 
-	// implement those methods inside the DAOImpl
+	public void generateReportCardDAO(Student student, Professor professor);
+	public void addProfessorDAO(Professor professor);
+	public void approveStudentRegistrationDAO(Student student);
+	public void addCourseDAO(Course course);
+	public void removeCourseDAO(int id);
+	public void updateCourseDAO(int id, String name, String description);
+	public void checkAvailabilityDAO(int id);
+	public void viewCourses(Student student);
+	
 }
